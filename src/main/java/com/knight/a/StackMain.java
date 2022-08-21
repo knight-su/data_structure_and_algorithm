8a83119914f178337d29b1f05941db942895083e
suxin <837081248@qq.com>
2022-08-22 00:32:07 +0800

    提交3

---
+++ src/main/java/com/knight/a/StackMain.java
@@ -0,0 +1,19 @@
+package com.knight.a;
+
+import java.util.Queue;
+import java.util.Stack;
+
+public class StackMain {
+
+
+
+    public static void main(String[] args) {
+        new Stack<Integer>();
+
+        Integer[] a= {1,2,3,4,5};
+        a = new Integer[1];
+        System.arraycopy(a,5,a,4,1);
+
+
+    }
+}
